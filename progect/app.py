180ce979002efa92e0633b4b014663cc60416e57
c0a23056d1 <c0a23056d1@edu.teu.ac.jp>
2024-12-12 18:46:05 +0900

    登録ページ

---
+++ progect/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for  
+import sqlite3  
+  
+app = Flask(__name__)  
+  
+def init_db():  
+    conn = sqlite3.connect('database.db')  
+    c = conn.cursor()  
+    c.execute('''  
+        CREATE TABLE IF NOT EXISTS products (  
+            id INTEGER PRIMARY KEY AUTOINCREMENT,  
+            category TEXT NOT NULL  
+        )  
+    ''')  
+    conn.commit()  
+    conn.close()  
+  
+@app.before_first_request  
+def setup():  
+    init_db()  
+  
+@app.route('/', methods=['GET', 'POST'])  
+def index():  
+    if request.method == 'POST':  
+        category = request.form['category']  
+        conn = sqlite3.connect('database.db')  
+        c = conn.cursor()  
+        c.execute('INSERT INTO products (category) VALUES (?)', (category,))  
+        conn.commit()  
+        conn.close()  
+        return redirect(url_for('index'))  
+      
+    conn = sqlite3.connect('database.db')  
+    c = conn.cursor()  
+    c.execute('SELECT * FROM products')  
+    products = c.fetchall()  
+    conn.close()  
+      
+    return render_template('index.html', products=products)  
+  
+if __name__ == '__main__':  
+    app.run(debug=True)  
